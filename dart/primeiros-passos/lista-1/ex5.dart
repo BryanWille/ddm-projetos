209bbe91bd982d5a3c4dbb38f3d8f51e19d69300
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-09-03 18:19:52 -0300

    Refatorando pastas

--- dart/primeiros-passos/lista-1/ex5.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print("Digite a temperatura em F°: ");
-  double f = double.parse(stdin.readLineSync()!);
-  double c = ((f - 32) / 9) * 5;
-  print("Temperatura $c C°");
-}
