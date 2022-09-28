4aca55aa91de211e7a33dc80e4bb1ad5ace427de
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-09-28 20:53:53 -0300

    Testing class Branch

--- dart/poo/lista-4-melhorado/main.dart
+++ dart/poo/lista-4-melhorado/main.dart
@@ -1,4 +1,5 @@
 import 'BankAccount.dart';
+import 'Branch.dart';
 
 void main(){
 
@@ -24,14 +25,22 @@ void main(){
   // ================= Transference Tests ================= // 
   BankAccount Bryan = new BankAccount(numberAccount: 1, clientName: "Bryan", balance: 100, creditScore: 100);
   BankAccount Wille = new BankAccount(numberAccount: 2, clientName: "Wille", balance: 100, creditScore: 100);
+  BankAccount Noe = new BankAccount(numberAccount: 3, clientName: "Noe", balance: 100, creditScore: 100);
+
+  Branch main = new Branch(branchName: "IDK", branchNumber: 0);
+  main.addAccount(Bryan);
+  main.addAccount(Wille);
+  main.addAccount(Noe);
+
+  print(main);
 
   // 1: Bryan.transfer(Bryan, Wille, 50);
   // 2: Bryan.transfer(Bryan, Wille, 100);
   
   // 3:
-  Bryan.transfer(Bryan, Wille, 250);
-  print(Bryan);
-  print(Wille);
+  //Bryan.transfer(Bryan, Wille, 250);
+  //print(Bryan);
+  //print(Wille);
 
 
 }
